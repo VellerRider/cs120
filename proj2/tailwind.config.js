e6fbbccc73c930946da7d1300d0c5e94d73146fc
Raw <108172547+Thigraw@users.noreply.github.com>
2024-10-28 02:00:52 -0400

    remove next.js part

---
+++ proj2/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
